97222c089b83fa11c1e34ea9d11e1bd7bdc4dfd1
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2022-10-16 15:28:09 +0700

    [16/10][Minh Hung] get user data

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -5,6 +5,23 @@ const authRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const { json } = require('express');
 
+const auth = async(req, res, next) => {
+    try {
+        const token = req.header('x-auth-token');
+        if (!token) return res.status(401).json({ msg: "No auth token, acceess denied!" });
+        const verified = jwt.verify(token, 'passwordKey');
+        if (!verified)
+            return res
+                .status(401)
+                .json({ msg: 'Token verification failed, authorization denied,' });
+        req.user = verified.id;
+        req.token = token;
+        next();
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+}
+
 
 authRouter.post('/api/signup', async(req, res) => {
     try {
@@ -43,6 +60,26 @@ authRouter.post('/api/signin', async(req, res) => {
     } catch (e) {
         res.status(500).json({ err: e.message });
     }
+});
+
+authRouter.post('/api/validToken', async(req, res) => {
+    try {
+        const token = req.header('X-auth-token');
+        if (!token) return res.json(false);
+        const verified = jwt.verify(token, "passwordkey");
+        if (!verified) return res.json(false);
+
+        const user = await User.findById(verified.id);
+        if (!user) return res.json(false);
+        res.json(true);
+    } catch (e) {
+        res.status(500).json({ err: e.message });
+    }
+});
+
+authRouter.get('/getUser', auth, async(req, res) => {
+    const user = await User.findById(req.user);
+    res.json({...user._doc, token: req.token });
 });
 
 module.exports = authRouter;
