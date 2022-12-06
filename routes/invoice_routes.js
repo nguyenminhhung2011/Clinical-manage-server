4d203f6388891e1f2f733ce7bc8c438c82b3cc02
Nguyễn Minh Hưng <90996598+minhunsocute@users.noreply.github.com>
2022-12-06 16:19:04 +0700

    [invoice] fetch invoice and insert invoice

---
+++ routes/invoice_routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const invoiceRouter = express.Router();
+const { json } = require('express');
+const Invoice = require('../models/invoice')
+
+
+invoiceRouter.get('/api/invoice/get_all', async(req, res) => {
+    try {
+        console.log("fecth all data invoice is called");
+        const invoice = await Invoice.find();
+        res.json(invoice);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+
+invoiceRouter.post('/api/invoice/add_invoice_medicine', async(req, res) => {
+    try {
+        console.log("add invoice medicine is called");
+        const { thumb, amount, status, createTime, title, medicineId, category } = req.body;
+        let invoice = new Invoice({
+            thumb,
+            amount,
+            status,
+            createTime,
+            title,
+            medicineId,
+            category,
+        });
+        invoice = await invoice.save();
+        res.json(invoice);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+module.exports = invoiceRouter;
